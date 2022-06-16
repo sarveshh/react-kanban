e4fb311728da1bdc46caa4f545ab83be016bd20b
Sarvesh Patil <sarveshp273@gmail.com>
2022-06-16 14:14:28 +0530

    Added cardinfo edit component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,33 +5,43 @@ import { AiFillDelete } from "react-icons/ai";
 import { BsCheck2Circle } from "react-icons/bs";
 import { CardContainer, CardFooter, CardHeader } from "./CardStyles";
 import Tags from "../Tags/Tags";
+import CardInfo from "../CardInfo/CardInfo";
 
 const Card = (props) => {
+  const [showCardInfo, setShowCardInfo] = React.useState(false);
   return (
-    <CardContainer
-      draggable
-      onDragEnd={() => props.handleCardDragLeave(props.card?.id, props.boardId)}
-      onDragEnter={() =>
-        props.handleCardDragEnter(props.card?.id, props.boardId)
-      }
-    >
-      {props.card?.tags?.map((tag, index) => (
-        <Tags key={index} text={tag.text} color={tag.color} />
-      ))}
-      <CardHeader>
-        <Typography variant="h5">{props.card.title}</Typography>
-        <Box>
-          <MdEdit />
-          <AiFillDelete
-            onClick={() => props.removeCard(props.card?.id, props.boardId)}
-          />
-        </Box>
-      </CardHeader>
-      <CardFooter className="card_footer">
-        <Typography variant="body1">{props.card.date}</Typography>
-        <BsCheck2Circle />
-      </CardFooter>
-    </CardContainer>
+    <>
+      {showCardInfo && (
+        <CardInfo onClose={() => setShowCardInfo(false)} open={showCardInfo} />
+      )}
+      <CardContainer
+        draggable
+        onDragEnd={() =>
+          props.handleCardDragLeave(props.card?.id, props.boardId)
+        }
+        onDragEnter={() =>
+          props.handleCardDragEnter(props.card?.id, props.boardId)
+        }
+        onClick={() => setShowCardInfo(true)}
+      >
+        {props.card?.tags?.map((tag, index) => (
+          <Tags key={index} text={tag.text} color={tag.color} />
+        ))}
+        <CardHeader>
+          <Typography variant="h5">{props.card.title}</Typography>
+          <Box>
+            <MdEdit />
+            <AiFillDelete
+              onClick={() => props.removeCard(props.card?.id, props.boardId)}
+            />
+          </Box>
+        </CardHeader>
+        <CardFooter className="card_footer">
+          <Typography variant="body1">{props.card.date}</Typography>
+          <BsCheck2Circle />
+        </CardFooter>
+      </CardContainer>
+    </>
   );
 };
 
